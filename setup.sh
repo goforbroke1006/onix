29336716b4f44efd4a456b40e1dca40f8691f188
goforbroke <go.for.broke1006@gmail.com>
2022-03-12 17:34:13 +0300

    test github actions

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 sudo curl -fsSL -o /usr/local/bin/dbmate https://github.com/amacneil/dbmate/releases/latest/download/dbmate-linux-amd64
 sudo chmod +x /usr/local/bin/dbmate
 
-go get -u github.com/deepmap/oapi-codegen/cmd/oapi-codegen
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.44.2
+go install github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.9.1
 go install github.com/golang/mock/mockgen@v1.6.0
 
 sudo apt-get install -y gcc g++ make
