5fd96963c79204e035572173f1b2984aa910a715
goforbroke <go.for.broke1006@gmail.com>
2022-04-13 18:55:13 +0300

    fix register image

--- .build/register/entrypoint.sh
+++ .build/register/entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-if [[ -z $BASE_URL ]]; then
+if [[ -z ${BASE_URL} ]]; then
   echo "ERROR: specify BASE_URL env"
   exit 1
 fi
 
-if [[ -z $SERVICE_NAME ]]; then
+if [[ -z ${SERVICE_NAME} ]]; then
   echo "ERROR: specify SERVICE_NAME env"
   exit 1
 fi
 
-if [[ -z $RELEASE_NAME ]]; then
+if [[ -z ${RELEASE_NAME} ]]; then
   echo "ERROR: specify RELEASE_NAME env"
   exit 1
 fi
 
-curl -X GET "$BASE_URL/api/system/register?service_name=$SERVICE_NAME&release_name=$RELEASE_NAME"
+url="${BASE_URL}/api/system/register?service_name=${SERVICE_NAME}&release_name=${RELEASE_NAME}"
+echo "${url}"
+curl -X GET "${url}"
