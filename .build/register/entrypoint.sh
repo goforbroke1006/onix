c36aac3700b52deab316055b29893b3589c1539c
goforbroke <go.for.broke1006@gmail.com>
2022-04-18 12:31:33 +0300

    change env var names for register image

--- .build/register/entrypoint.sh
+++ .build/register/entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [[ -z ${BASE_URL} ]]; then
-  echo "ERROR: specify BASE_URL env"
+if [[ -z ${ONIX_REGISTER_BASE_URL} ]]; then
+  echo "ERROR: specify ONIX_REGISTER_BASE_URL env"
   exit 1
 fi
 
-if [[ -z ${SERVICE_NAME} ]]; then
-  echo "ERROR: specify SERVICE_NAME env"
+if [[ -z ${ONIX_REGISTER_SERVICE_NAME} ]]; then
+  echo "ERROR: specify ONIX_REGISTER_SERVICE_NAME env"
   exit 1
 fi
 
-if [[ -z ${RELEASE_NAME} ]]; then
-  echo "ERROR: specify RELEASE_NAME env"
+if [[ -z ${ONIX_REGISTER_RELEASE_NAME} ]]; then
+  echo "ERROR: specify ONIX_REGISTER_RELEASE_NAME env"
   exit 1
 fi
 
-url="${BASE_URL}/api/system/register?service_name=${SERVICE_NAME}&release_name=${RELEASE_NAME}"
+url="${ONIX_REGISTER_BASE_URL}/api/system/register?service_name=${ONIX_REGISTER_SERVICE_NAME}&release_name=${ONIX_REGISTER_RELEASE_NAME}"
 echo "${url}"
 curl -X GET "${url}"
