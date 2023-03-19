d9b12c8abfdcca552db0a34f986aae5aa8ae22e2
goforbroke <go.for.broke1006@gmail.com>
2023-03-19 17:56:01 +0200

    refactoring

--- api/stub_prometheus/gen.go
+++ api/stub_prometheus/gen.go
@@ -1,4 +1,4 @@
 package stubprometheus
 
-//go:generate oapi-codegen --package=stubprometheus --generate=types,skip-prune  -o ./types.generated.go  openapi.yaml
-//go:generate oapi-codegen --package=stubprometheus --generate=server,skip-prune -o ./server.generated.go openapi.yaml
+//go:generate oapi-codegen --package=stubprometheus --generate=types,skip-prune  -o ./types.gen.go  openapi.yaml
+//go:generate oapi-codegen --package=stubprometheus --generate=server,skip-prune -o ./server.gen.go openapi.yaml
